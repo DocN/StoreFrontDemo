54b55f49b7fe2f582899dda089f4f53c11c2c197
DocN <031247ryan@gmail.com>
2018-11-26 21:12:23 -0800

    created DB Context for code first

---
+++ StoreFront/StoreFront/Data/StoreFrontContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using StoreFront.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoreFront.Data
+{
+    public class StoreFrontContext : IdentityDbContext<IdentityUser>
+    {
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Store> Store { get; set; }
+        public DbSet<Publisher> Publisher { get; set; }
+
+        public StoreFrontContext(DbContextOptions<StoreFrontContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            #region "Seed Data"
+
+            builder.Entity<IdentityRole>().HasData(
+                new { Id = "1", Name = "Admin", NormalizedName = "ADMIN" },
+                new { Id = "2", Name = "Customer", NormalizedName = "CUSTOMER" }
+            );
+
+            #endregion
+        }
+    }
+}
